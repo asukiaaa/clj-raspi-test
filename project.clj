c2f344e38227516d3d08bc6105c8847e57eba9b2
Asuki Kono <asukiaaa@gmail.com>
2017-02-12 13:49:48 +0000

    Try to control gpio

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject raspi-test "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [gpio "0.2.0"]]
+  :main raspi-test.core)
--- src/raspi_test/core.clj
+++ src/raspi_test/core.clj
@@ -1,1 +1,28 @@
-(ns raspi-test.core)
+(ns raspi-test.core
+  (:require [gpio.core :as gpio]))
+
+(def button-pin 3)
+(def led1-pin 4)
+(def led2-pin 5)
+
+(defn toggle-led []
+  (let [button-value (gpio/read-value button-pin)]
+    (prn :button-value button-value)
+    (if (= :high button-value)
+      (gpio/write-value led1-pin true)
+      ;;(gpio/toggle-value led1-pin)
+      (gpio/toggle-value led2-pin))
+    (delay 1000)))
+
+(defn -main [& args]
+  ;(gpio/open-pin button-pin)
+  ;(gpio/open-pin led1-pin)
+  ;(gpio/open-pin led2-pin)
+  (prn :button (gpio/read-value button-pin))
+  (gpio/set-direction led1-pin :out)
+  (gpio/set-direction led2-pin :out)
+  (toggle-led))
+
+;; runtime shutdown hook
+;; tools.namespace
+;; with-open
