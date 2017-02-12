78407d44306e0ec38aa776cffd652a3dbb66353c
Asuki Kono <asukiaaa@gmail.com>
2017-02-12 05:18:12 +0000

    Initial commit

---
+++ src/raspi_test/core.clj
@@ -0,0 +1,1 @@
+(ns raspi-test.core)
---
+++ test/raspi_test/test/core.clj
@@ -0,0 +1,6 @@
+(ns raspi-test.test.core
+  (:use [raspi-test.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
