57da32827d96441a532d64530c3f7901da7c5fca
Asuki Kono <asukiaaa@gmail.com>
2017-02-15 20:47:50 +0900

    Control with using clj-gpio

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject raspi-test "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [gpio "0.2.0"]]
+                 [clj-gpio "0.2.0"]]
   :main raspi-test.core)
--- src/raspi_test/core.clj
+++ src/raspi_test/core.clj
@@ -1,27 +1,33 @@
 (ns raspi-test.core
   (:require [gpio.core :as gpio]))
 
-(def button-pin 3)
-(def led1-pin 4)
-(def led2-pin 5)
+(def button-port (gpio/open-port 22))
+(def led1-port (gpio/open-port 23))
+(def led2-port (gpio/open-port 24))
+
+(defn close-ports []
+  (prn :close-ports)
+  (gpio/close! button-port)
+  (gpio/close! led1-port)
+  (gpio/close! led2-port))
 
 (defn toggle-led []
-  (let [button-value (gpio/read-value button-pin)]
+  (let [button-value (gpio/read-value button-port)]
     (prn :button-value button-value)
     (if (= :high button-value)
-      (gpio/write-value led1-pin true)
-      ;;(gpio/toggle-value led1-pin)
-      (gpio/toggle-value led2-pin))
+      (gpio/toggle! led1-port)
+      (gpio/toggle! led2-port))
     (delay 1000)))
 
 (defn -main [& args]
-  ;(gpio/open-pin button-pin)
-  ;(gpio/open-pin led1-pin)
-  ;(gpio/open-pin led2-pin)
-  (prn :button (gpio/read-value button-pin))
-  (gpio/set-direction led1-pin :out)
-  (gpio/set-direction led2-pin :out)
-  (toggle-led))
+  (.addShutdownHook (Runtime/getRuntime)
+                    (Thread. close-ports))
+  (prn :start)
+  (gpio/set-direction! led1-port :out)
+  (gpio/set-direction! led2-port :out)
+  (gpio/write-value! led1-port :high)
+  (gpio/write-value! led2-port :low)
+  (while true (toggle-led)))
 
 ;; runtime shutdown hook
 ;; tools.namespace
